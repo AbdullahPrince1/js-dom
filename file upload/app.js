e87d89a059d757699eddb97ada3d0fbd9318e1b1
AbdullahPrince <princex660@gmail.com>
2025-08-22 16:47:25 +0600

    another bomb

---
+++ file upload/app.js
@@ -0,0 +1,7 @@
+const btn = document.querySelector("button");
+const input = document.querySelector("input");
+const img = document.querySelector("img");
+
+btn.addEventListener("click", function () {
+  input.click();
+});
